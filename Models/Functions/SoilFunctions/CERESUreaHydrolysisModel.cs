846447285f4e64166014fb033226cb44363f1244
Heather Pasley <heather.pasley@csiro.au>
2026-07-02 16:41:16 +1000

    Changed the WF for Hydrolysis to match Myers

--- Models/Functions/SoilFunctions/CERESUreaHydrolysisModel.cs
+++ Models/Functions/SoilFunctions/CERESUreaHydrolysisModel.cs
@@ -18,6 +18,17 @@ namespace Models.Functions
         [Link]
         Organic organic = null;
 
+        private double[] wf;
+
+        [Link]
+        Soil soil = null;
+
+        [Link]
+        Water water = null;
+
+        [Link]
+        IPhysical physical = null;
+
         [Link]
         Chemical chemical = null;
 
@@ -30,6 +41,34 @@ namespace Models.Functions
 
         /// <summary>Gets the value.</summary>
         /// <value>The value.</value>
+        [EventSubscribe("WaterChanged")]
+        private void OnWaterChanged(object sender, EventArgs e)
+        {
+            double[] SW = water.Volumetric;
+            double[] AD = physical.AirDry;
+            double[] LL15 = physical.LL15;
+            double[] DUL = physical.DUL;
+            double[] SAT = physical.SAT;
+            if (wf == null)
+                wf = new double[SW.Length];
+            for (int i = 0; i < SW.Length; i++)
+            {
+                if (SW[i] < LL15[i])
+                {
+                        wf[i] = Math.Min(1, MathUtilities.Divide(SW[i] - AD[i], LL15[i] - AD[i], 0.0));
+                }
+                else if (SW[i] < DUL[i])
+                {
+                    wf[i]=1;
+                }
+                else
+                    wf[i] = 1 - 0.5 * MathUtilities.Divide(SW[i] - DUL[i], SAT[i] - DUL[i], 0.0);
+            }
+        }
+    
+
+        /// <summary>Gets the value.</summary>
+        /// <value>The value.</value>
         public double Value(int arrayIndex = -1)
         {
             if (arrayIndex == -1)
@@ -37,8 +76,7 @@ namespace Models.Functions
 
             double potentialRate = -1.12 + 1.31 * organic.Carbon[arrayIndex] + 0.203 * chemical.PH[arrayIndex] - 0.155 * organic.Carbon[arrayIndex] * chemical.PH[arrayIndex];
             potentialRate = MathUtilities.Bound(potentialRate, 0, 1);
-
-            double WF = MathUtilities.Bound(CERESWF.Value(arrayIndex) + 0.2, 0, 1);
+            double WF = MathUtilities.Bound(wf[arrayIndex], 0, 1);
             double TF = MathUtilities.Bound(soilTemperature.Value[arrayIndex] / 40 + 0.2, 0, 1);
             double rateModifer = Math.Min(WF, TF);
 
